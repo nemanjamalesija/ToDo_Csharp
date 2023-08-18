d42825317eee4b90c3fd1bb5b57f13cde3ccfdf3
nemanjamalesija <malesija13@gmail.com>
2023-08-18 21:36:11 +0200

    Complete app

--- Todo/Todo/Program.cs
+++ Todo/Todo/Program.cs
@@ -1,2 +1,74 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+/*
+if (userChoice == "A")
+{
+
+    stringList.Add(userChoice);
+    Console.WriteLine("Todo added");
+
+}
+
+Console.ReadKey();
+
+*/
+
+
+public class TodoList
+{
+    List<string> stringList = new List<string>();
+
+    public static void Main()
+    {
+        TodoList todoList = new TodoList();
+        todoList.RunTodoApp();
+    }
+
+    public void RunTodoApp()
+    {
+        while (true) // Run indefinitely
+        {
+            Console.WriteLine("Hello, what would you want to do ?");
+            Console.WriteLine("[S]ee all todos");
+            Console.WriteLine("[A]dd todo");
+            Console.WriteLine("[R]emove todo");
+            Console.WriteLine("[E]xit");
+
+            string userChoice = Console.ReadLine();
+
+            if (userChoice == "S")
+            {
+                PrintTodos();
+            }
+            else if (userChoice == "A")
+            {
+                AddTodo();
+            }
+            else if (userChoice == "E")
+            {
+                break; // Exit the loop and end the application
+            }
+        }
+    }
+
+    void PrintTodos()
+    {
+        for (int i = 0; i < stringList.Count; i++)
+        {
+            Console.WriteLine(stringList[i]);
+        }
+    }
+
+    void AddTodo()
+    {
+        Console.WriteLine("Enter the new todo:");
+        string newTodo = Console.ReadLine();
+        stringList.Add(newTodo);
+        Console.WriteLine("Todo added");
+    }
+}
