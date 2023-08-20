14d490a244c737b1454e8b7a9d451e3072c219e9
nemanjamalesija <malesija13@gmail.com>
2023-08-20 23:13:51 +0200

    Clean code

--- Projects/Todo/Program.cs
+++ Projects/Todo/Program.cs
@@ -1,8 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
+﻿
 using System;
-
+/*
 public class TodoList
 {
     List<string> stringList = new List<string>();
@@ -169,7 +167,7 @@ public class TodoList
         }
 
         
-        for (var i=0; i < stringList.Count; i++)
+        for (var i = 0; i < stringList.Count; i++)
         {
             if (i == parsedTodoIndexInput - 1)
             {
@@ -187,7 +185,65 @@ public class TodoList
 }
 
 
+/*/
 
 
+/*
+Define a Dog class. Each Dog will have a name(string), breed (string), and weight (int) fields.
 
+* This class will have two constructors:
+- The first takes name, breed, and weight parameters (in this order).
+- The second takes only the name and weight parameters (in this order). 
+- In this case, the breed should be assigned the "mixed-breed" value.
+
+* This class will expose a single public method called Describe, returning a string describing the dog.
+
+* For a dog named Lucky, with the breed "german shepherd" and weight of 40, this method should return:
+- "This dog is named Lucky, it's a german shepherd, and it weighs 40 kilograms, so it's a large dog."
+
+*For a dog named Tina, with the breed "shar pei" and weight of 25, this method should return:
+- "This dog is named Tina, it's a shar pei, and it weighs 25 kilograms, so it's a medium dog."
+
+The rule for translating the dog's weight to the weight description is as follows:
+- For dogs lighter than 5 kilos, it is "tiny".
+- For dogs of a weight of 5 kilos but less than 30 kilos, it is "medium".
+- For other dogs, it is "large".
+ * */
+
+var Larry = new Dog("Larry", "Sheppard", 25);
+var Tina = new Dog("Tina", "shar-pei", 3);
+var Sosomus = new Dog("Sosomus", 50);
+
+Console.WriteLine(Tina.Describe());
+Console.WriteLine(Larry.Describe());
+Console.WriteLine(Sosomus.Describe());
+
+public class Dog
+{
+    private string _name;
+    private string _breed;
+    private int _weight;
+
+    public Dog(string name, string breed, int weight)
+    {
+        _name = name;
+        _breed = breed;
+        _weight = weight;
+    }
+
+    public Dog(string name, int weight) : this(name, "Mixed breed", weight) { }
+ 
+    
+    public string Describe()
+    {
+        string dogSize;
+
+        if (_weight < 5) dogSize = "tiny";
+        else if (_weight > 5 && _weight < 30) dogSize = "medium";
+        else dogSize = "large";
+
+        return $"Name of the dog is {_name}, its breed is {_breed}, it's weight is {_weight}. This is {dogSize} dog";
+    }
+
+}
 
