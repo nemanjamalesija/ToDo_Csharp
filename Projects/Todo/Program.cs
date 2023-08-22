168a5657ab6d54a1edcde47a430070d68835834b
nemanjamalesija <malesija13@gmail.com>
2023-08-22 19:27:48 +0200

    Move user input to a separate class

--- Projects/Todo/Program.cs
+++ Projects/Todo/Program.cs
@@ -1,162 +1,163 @@
-﻿
-using System;
+﻿public class TodoList
+    {
+        readonly List<string> StringList = new();
+        private char UserChoiceSARE { get; set; }
+        private char UserChoiceTODO { get; set; }
 
-public class TodoList
-{
-    readonly List<string> StringList = new(); 
-    private char UserChoiceSARE {  get; set; }
-    private char UserChoiceTODO { get; set; }
+
+        readonly UserInputManager InputManager = new();
 
     public static void Main()
-    {
-        TodoList todoList = new();
-        todoList.RunTodoApp();
-    }
-
-    public void RunTodoApp()
-    {
-        while (true) // Run indefinitely
         {
-             Console.WriteLine("What would you want to do ?");
-            
-             AskUserForActionSARE();
-            UserChoiceSARE = GetUserInput();
+            TodoList todoList = new();
+            todoList.RunTodoApp();
+        }
 
-            if (!(ValidateUserChoiceSARE(UserChoiceSARE)))
+        public void RunTodoApp()
+        {
+            while (true) // Run indefinitely
             {
+                Console.WriteLine("What would you want to do ?");
 
-                Console.WriteLine("Please select correct input.");
                 AskUserForActionSARE();
                 UserChoiceSARE = GetUserInput();
-            }
 
-            if (UserChoiceSARE.ToString().ToUpper() == "S")
-            {
-                PrintTodos();
-            }
-              if (UserChoiceSARE.ToString().ToUpper() == "A")
-            {
-                Console.WriteLine("Enter the new todo:");
-                string newTodo = GetNewTodo();
-                AddTodo(newTodo);
-            }
-
-            if (UserChoiceSARE.ToString().ToUpper() == "R")
-            {
-                if (StringList.Count == 0)
+                if (!ValidateUserChoiceSARE(UserChoiceSARE))
                 {
 
-                    Console.WriteLine("You have no todos.");
-                    RunTodoApp();
-                    return;
+                    Console.WriteLine("Please select correct input.");
+                    AskUserForActionSARE();
+                    UserChoiceSARE = GetUserInput();
                 }
 
-                Console.WriteLine("Select the index of todo you want to remove:");
-                PrintTodos();
-                UserChoiceTODO = GetUserInput();
-                RemoveTodo(UserChoiceTODO);
-            }
+                if (UserChoiceSARE.ToString().ToUpper() == "S")
+                {
+                    PrintTodos();
+                }
+                if (UserChoiceSARE.ToString().ToUpper() == "A")
+                {
+                    Console.WriteLine("Enter the new todo:");
+                    string newTodo = GetNewTodo();
+                    AddTodo(newTodo);
+                }
 
-            if (UserChoiceTODO.ToString().ToUpper() == "E")
+                if (UserChoiceSARE.ToString().ToUpper() == "R")
+                {
+                    if (StringList.Count == 0)
+                    {
+
+                        Console.WriteLine("You have no todos.");
+                        RunTodoApp();
+                        return;
+                    }
+
+                    Console.WriteLine("Select the index of todo you want to remove:");
+                    PrintTodos();
+                    UserChoiceTODO = GetUserInput();
+                    RemoveTodo(UserChoiceTODO);
+                }
+
+                if (UserChoiceTODO.ToString().ToUpper() == "E")
+                {
+                    break; // Exit the loop and end the application
+                }
+            }
+        }
+
+        static char GetUserInput()
+        {
+            ConsoleKeyInfo keyPressed = Console.ReadKey(true);
+            return keyPressed.KeyChar;
+        }
+
+        static void AskUserForActionSARE()
+        {
+
+            Console.WriteLine("[S]ee all todos");
+            Console.WriteLine("[A]dd todo");
+            Console.WriteLine("[R]emove todo");
+            Console.WriteLine("[E]xit");
+            Console.WriteLine();
+
+        }
+
+        static string GetNewTodo()
+        {
+            string newTodo = Console.ReadLine() ?? "";
+            return newTodo;
+        }
+
+        static bool ValidateUserChoiceSARE(char input)
+        {
+            string comp = input.ToString().ToUpper();
+
+            switch (comp)
             {
-                break; // Exit the loop and end the application
+                case "S":
+                case "A":
+                case "R":
+                case "E":
+                    return true;
+
+                default:
+                    return false;
             }
-        }
-    }
 
-   static char GetUserInput()
-    {
-        ConsoleKeyInfo keyPressed = Console.ReadKey(true);
-        return keyPressed.KeyChar;
-    }
-
-    void AskUserForActionSARE()
-    {
-   
-        Console.WriteLine("[S]ee all todos");
-        Console.WriteLine("[A]dd todo");
-        Console.WriteLine("[R]emove todo");
-        Console.WriteLine("[E]xit");
-        Console.WriteLine();
-
-    }
-
-   static string GetNewTodo ()
-    {
-        string newTodo = Console.ReadLine() ?? "";
-        return newTodo;
-    }
-
-     static bool ValidateUserChoiceSARE(char input)
-    {
-      string comp = input.ToString().ToUpper();
-
-        switch (comp)
-        {
-            case "S":
-            case "A":
-            case "R":
-            case "E":
-                return true;
-
-            default:
-                return false;
         }
 
-    }
-
-    void PrintTodos()
-    {
-        if (StringList.Count == 0)
+        void PrintTodos()
         {
+            if (StringList.Count == 0)
+            {
 
-            Console.WriteLine("You have no todos.");
+                Console.WriteLine("You have no todos.");
+                Console.WriteLine();
+                return;
+            }
+
+            for (int i = 0; i < StringList.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {StringList[i]}");
+            }
+
             Console.WriteLine();
-            return;
         }
 
-        for (int i = 0; i < StringList.Count; i++)
+        void AddTodo(string todo)
         {
-            Console.WriteLine($"{i + 1}. {StringList[i]}");
+
+            if (todo == "")
+            {
+                Console.WriteLine("The todo input cannot be empty.");
+                Console.WriteLine();
+                string newTodo = GetNewTodo();
+                AddTodo(newTodo);
+
+            }
+
+            if (StringList.Contains(todo))
+            {
+
+                Console.WriteLine("This todo is already in your list of todos.");
+                Console.WriteLine();
+                string newTodo = GetNewTodo();
+                AddTodo(newTodo);
+
+            }
+
+            StringList.Add(todo);
+            Console.WriteLine("Todo added.");
+            Console.WriteLine();
         }
 
-        Console.WriteLine();
-    }
-
-    void AddTodo(string todo)
-    {
-       
-        if (todo == "")
+        void RemoveTodo(char todoIndex)
         {
-            Console.WriteLine("The todo input cannot be empty.");
-            Console.WriteLine();
-            string newTodo = GetNewTodo();
-            AddTodo(newTodo);
 
-        }
-
-        if (StringList.Contains(todo)) {
-
-            Console.WriteLine("This todo is already in your list of todos.");
-            Console.WriteLine();
-            string newTodo = GetNewTodo();
-            AddTodo(newTodo);
-
-        }
-
-        StringList.Add(todo);
-        Console.WriteLine("Todo added.");
-        Console.WriteLine();
-    }
-
-    void RemoveTodo(char todoIndex)
-    {
-  
             string removedTodo = "";
 
             bool successParse = int.TryParse(todoIndex.ToString(), out int number);
 
-            if (!(successParse))
+            if (!successParse)
             {
 
                 Console.WriteLine("Not a number. Please provide valid input.");
@@ -193,7 +194,3 @@ public class TodoList
         }
 
     }
-
-
-
-
---
+++ Projects/Todo/UserInputManager.cs
@@ -0,0 +1,46 @@
+﻿
+class UserInputManager 
+{
+
+    static string GetNewTodo()
+    {
+        string newTodo = Console.ReadLine() ?? "";
+        return newTodo;
+    }
+
+    public bool ValidateUserChoiceSARE(char input)
+    {
+        string comp = input.ToString().ToUpper();
+
+        switch (comp)
+        {
+            case "S":
+            case "A":
+            case "R":
+            case "E":
+                return true;
+
+            default:
+                return false;
+        }
+
+    }
+
+    public char GetUserInput()
+    {
+        ConsoleKeyInfo keyPressed = Console.ReadKey(true);
+        return keyPressed.KeyChar;
+    }
+
+    public void AskUserForActionSARE()
+    {
+
+        Console.WriteLine("[S]ee all todos");
+        Console.WriteLine("[A]dd todo");
+        Console.WriteLine("[R]emove todo");
+        Console.WriteLine("[E]xit");
+        Console.WriteLine();
+
+    }
+
+}
